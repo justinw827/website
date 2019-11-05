64f6e5c1485d2294660a94aac04ffddeb78ffa01
Justin Wang <justinw0827@gmail.com>
2019-11-05 16:21:14 -0500

    Added canvas

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import React from 'react';
 import './App.css';
 
 import NavBar from './components/NavBar';
+import Canvas from './components/Canvas';
+import logo from './images/logo.png'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-library.add(fab, faCoffee)
+library.add(fab )
 
 function App() {
   return (
    <div className="App">
       <NavBar />
-      // <div className="drop-shadow"></div>
       <div className="bgimg-1">
-         <h1>JUSTIN WANG</h1>
+         <img src={logo} alt="JUSTIN WANG" height="250" width="250"/>
       </div>
       <div style={{height: "200px", backgroundColor: "#FFF", textAlign: "center", lineHeight: "200px", fontSize: "36pt"}}>Coming Soon</div>
       <div className="bgimg-2"></div>
       <div style={{height: "200px", backgroundColor: "#FFF", textAlign: "center", lineHeight: "200px", fontSize: "36pt"}}>TBD</div>
-      <div className="bgimg-1"></div>
+      <Canvas />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import { Menu } from 'semantic-ui-react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -12,7 +12,10 @@ class NavBar extends Component {
 	   // }
 
 	  	return (
-			<Menu size="large" className="navbar" style={{backgroundColor: "rgb(136, 77, 77)"}}>
+			<Menu size="large" className="navbar" style={{backgroundColor: "rgb(49, 113, 150)"}}>
+				<Menu.Item position="left">
+					<h1 style={{color: "#FFF"}}>JW</h1>
+				</Menu.Item>
 				<Menu.Item position="right">
 					<a href="https://www.linkedin.com/in/justin-wang-profile/"><FontAwesomeIcon style={{color: "#FFF"}} icon={['fab', 'linkedin']} size="2x"/></a>
 					<a href="https://github.com/justinw827"><FontAwesomeIcon style={{color: "#FFF", marginLeft: "1em"}} icon={['fab', 'github']} size="2x"/></a>
